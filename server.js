d16517187f6d6f8758334e25c81bf176f75a4ba2
nietonicolasleon <nietonicolasleon@gmail.com>
2022-05-26 15:57:35 -0300

    Views setted, Assest created and loaded

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
 const app = express();
 
 dotenv.config({path:'config.env'})
@@ -8,6 +11,19 @@ const PORT = process.env.PORT||8080;
 
 //log request
 app.use(morgan('tiny'));
+
+//request del body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//seteo del view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+//Cargar assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")))
+app.use("/css", express.static(path.resolve(__dirname, "assets/img")))
+app.use("/css", express.static(path.resolve(__dirname, "assets/js")))
+
 
 app.get('/',(req, res)=>{
     res.send("Crud Application");
